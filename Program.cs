e6ff422e93969c657ca1de4bb3524ff283194e8f
unknown <adil.erzhan@gmail.com>
2022-10-26 17:35:05 +0300

    HomeworkSem3

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿//Напишите программу которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+/*int DefinePalindromOrNot(int num)
+{
+if (n=> 10000 && n < 100000)
+int FirstDigit = num 1 10000;
+int LastDigit = num % 10;
+} 
+Console.Write("Input five digit number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (FirstDigit = LastDigit)
+Console.WriteLine("This is palindrom");
+else
+Console.WriteLine("This is NOT palindrom");*/
+    
+
+// Пытался хотя бы первыми отделаться) не получилось. Думал долго ответ не нашел время поджимает
+
+
+
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+/*double Distance3D(double ax, double bx, double ay, double by, double az, double bz)
+{
+    return Math.Sqrt(Math.Pow((ax - bx),2) + Math.Pow((ay - by),2) + Math.Pow((az - bz),2));
+}
+Console.Write("Input ax-coordinate: ");
+double ax = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input ay-coordinate: ");
+double ay = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input az-coordinate: ");
+double az = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input bx-coordinate: ");
+double bx = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input by-coordinate: ");
+double by = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input bz-coordinate: ");
+double bz = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(Distance3D(ax, bx, ay, by, az, bz));*/
+
+//Напишите программу, которая принимает  на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+/*void WriteCube(int n)
+{
+    int count = 1;
+    while (count <= n)
+    {
+        Console.WriteLine(Math.Pow(count,3));
+        count++;
+    }
+}
+Console.WriteLine("Input N number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+WriteCube(n);*/               
